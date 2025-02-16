1ee13d5759fe624bc6935c21d08a44c722dce254
artem <maffiasng@gmail.com>
2025-02-17 01:38:33 +0300

    1.Запрос без параметра method! Wrong method provided 2.Запрос 'options' не из списка! Wrong HTTP method 3.Запрос с правильным значением method {"success":"!"} 4.Цикла проверяет все возможные сочетания реальных типов запроса и значений параметра method.

--- python_script.py
+++ python_script.py
@@ -15,3 +15,51 @@ for i, redirect in enumerate(response.history):
     print(f"Redirect #{i+1}:{redirect.url}") #выводим редиректы с изначальной  точки назначения
 
 print(f"Количество редиректов:{count_red} до Конечного URL:{response.url}")
+
+#______________________
+#Ex7: Запросы и методы
+#______________________
+
+url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+methods = ("POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS","GET")
+values = ["POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS","GET"]
+
+#1 Делает http-запрос любого типа без параметра method, описать что будет выводиться в этом случае.
+response = requests.request(method='post', url=url)
+print(f"Запрос без параметра method!\n{response.text} - {response.status_code}\n")
+
+#2 Делает http-запрос не из списка. Например, HEAD. Описать что будет выводиться в этом случае.
+response = requests.request(method='options', url=url)
+print(f"Запрос не из списка!\n{response.text} - {response.status_code}\n")
+
+#3  Делает запрос с правильным значением method. Описать что будет выводиться в этом случае.
+response = requests.request(method='POST', url=url, data={"method":"POST"})
+print(f"Запрос с правильным значением method!\n{response.text} - {response.status_code}\n")
+
+#4 Цикла проверяет все возможные сочетания реальных типов запроса и значений параметра method.
+for method in methods:
+    for value in values:
+        try:
+            response = requests.request(method=method, url=url, data ={"method": value})
+            if response.status_code == 200:
+                print(f"Метод: {method} \n"
+                      f"Полученный ответ: {response.json()}, Код ответа: {response.status_code} \n"
+                      f"Значание которое передаем: {value}\n")
+            if method == value:# если метод равен параметру method.
+                print(f"Метод:{method}\n"
+                      f"Полученный ответ:{response.text}, Код ответа:{response.status_code}\n"
+                      f"Значание которое передаем: {value}\n")
+        except requests.RequestException as e:
+            pass
+        try:
+            response = requests.request(method=method, url=url, params={"method": value})
+            if response.status_code == 200:
+                print(f"Метод: {method} \n"
+                      f"Полученный ответ: {response.text}, Код ответа: {response.status_code} \n"
+                      f"Значание которое передаем: {value}\n")
+            if method == value and method != "GET" and response.status_code != 400:
+                print(f"Метод:{method}\n"
+                      f"Полученный ответ:{response.text}, Код ответа:{response.status_code}\n"
+                      f"Значание которое передаем: { value}\n")
+        except requests.RequestException as e:
+            pass
